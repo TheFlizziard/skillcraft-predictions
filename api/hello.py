530de00ee1480170c918f6d1b50f51b8c2ab2f8f
AntoineBlanot <antoineblanot@gmail.com>
2021-12-26 17:14:53 +0100

    small changes in hello.py

--- api/hello.py
+++ api/hello.py
@@ -7,7 +7,8 @@ def index():
     return render_template('index.html')
 
 # To launch the hello world api, do in cmd: 
-# export FLASK_APP=api/hello
-# export FLASK_ENV=development
-# flask run
-# CTRL+C pour arreter 
+## export FLASK_APP=api/hello
+## export FLASK_ENV=development
+## flask run
+## CTRL+C to stop 
+# Go to http://127.0.0.1:5000/
