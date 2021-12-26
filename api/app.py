fac4a817069fd5d9f481c289a0f4899a478c8a2f
AntoineBlanot <antoineblanot@gmail.com>
2021-12-26 21:32:13 +0100

    base of flask api for predictions

--- api/app.py
+++ api/app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, url_for, redirect
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
+app.config['SECRET_KEY'] = 'key'
 
 # To launch the hello world api, do in cmd: 
 ## export FLASK_APP=api/app
@@ -12,3 +9,25 @@ def index():
 ## flask run
 ## CTRL+C to stop 
 # Go to http://127.0.0.1:5000/
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/prediction', methods=('GET', 'POST'))
+def prediction():
+    if request.method == 'POST':
+        feature1 = request.form['feature1']
+        feature2 = request.form['feature2']
+
+        if not feature1:
+            flash('feature1 is required !')
+        elif not feature2:
+            flash('feature2 is required !')
+        else:
+            print(f'Title : {feature1}')
+            print(f'Content : {feature2}')
+            return redirect(url_for('index'))
+
+    return render_template('prediction.html')
