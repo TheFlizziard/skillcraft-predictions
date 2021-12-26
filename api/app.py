87d203d6c77a9007b21703b82203c803f8db1d1e
AntoineBlanot <antoineblanot@gmail.com>
2021-12-26 17:45:30 +0100

    more details on flask api

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ def index():
     return render_template('index.html')
 
 # To launch the hello world api, do in cmd: 
-## export FLASK_APP=api/hello
+## export FLASK_APP=api/app
 ## export FLASK_ENV=development
 ## flask run
 ## CTRL+C to stop 
